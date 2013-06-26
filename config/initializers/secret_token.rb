18e51025b056353e5d1b3e7d52e2aae58d046cc6
Amit Paul <amit@evolvinx.com>
2013-06-26 19:12:05 +0600

    Amit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvolvinxCrm::Application.config.secret_token = '42f52b49238535219198276b3df9b1831fdc1af0f805ac16fa31697553ad0d4c8dfbc8e1518c6edc9db3088ff965bf8bb424c7848804e21182c1e891239e053e'
