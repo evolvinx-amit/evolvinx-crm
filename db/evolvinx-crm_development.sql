d125dc6e19cb6a56796caef5544f2b1dc792b1be
Amit Paul <amit@evolvinx.com>
2013-06-29 20:20:54 +0600

    amit paul

--- db/evolvinx-crm_development.sql
+++ db/evolvinx-crm_development.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2013 at 09:33 AM
+-- Generation Time: Jun 29, 2013 at 04:20 PM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS `roles` (
 
 INSERT INTO `roles` (`id`, `name`, `enabled`, `list_position`, `created_at`, `updated_at`) VALUES
 (1, 'Admin', 1, 1, '2013-06-18 12:03:03', '2013-06-18 12:03:03'),
-(2, 'Sales', 1, 2, '2013-06-18 12:03:03', '2013-06-18 12:03:03'),
-(3, 'Marketing', 1, 3, '2013-06-18 12:03:03', '2013-06-18 12:03:03');
+(2, 'Recruiter', 1, 2, '2013-06-18 12:03:03', '2013-06-18 12:03:03'),
+(3, 'Sales', 1, 3, '2013-06-18 12:03:03', '2013-06-18 12:03:03');
 
 -- --------------------------------------------------------
 
@@ -74,21 +74,22 @@ CREATE TABLE IF NOT EXISTS `users` (
   `state` varchar(255) DEFAULT NULL,
   `zip` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  `mobile` varchar(255) DEFAULT NULL,
   `fax` varchar(255) DEFAULT NULL,
-  `notes` text,
   `active` tinyint(1) DEFAULT '1',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `role_id`, `name`, `address`, `city`, `state`, `zip`, `phone`, `mobile`, `fax`, `notes`, `active`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@example.com', 'aaaaaa', 1, 'Company Admin', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, '2013-06-18 12:03:03', '2013-06-18 19:49:11');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role_id`, `name`, `address`, `city`, `state`, `zip`, `phone`, `fax`, `active`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'admin@example.com', '0b4e7a0e5fe84ad35fb5f95b9ceeac79', 1, 'Company Admin', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2013-06-18 12:03:03', '2013-06-18 19:49:11'),
+(2, 'recruiter', 'recruiter@example.com', '0b4e7a0e5fe84ad35fb5f95b9ceeac79', 2, 'Company recruiter', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2013-06-18 12:03:03', '2013-06-18 19:49:11'),
+(3, 'sales', 'sales@example.com', '0b4e7a0e5fe84ad35fb5f95b9ceeac79', 3, 'Company sales', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2013-06-18 12:03:03', '2013-06-18 19:49:11'),
+(18, 'asdfasd', 'test1@gmail.comaa', '0b4e7a0e5fe84ad35fb5f95b9ceeac79', 3, 'asdds', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2013-06-29 13:29:00', '2013-06-29 13:29:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
